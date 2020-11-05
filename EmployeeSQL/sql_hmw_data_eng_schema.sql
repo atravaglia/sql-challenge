0dd4bda70ad6307ccd952b92a23d3496aba56371
Alessandro Travaglia <atravaglia@gmail.com>
2020-11-04 23:17:52 -0500

    sql_homework_final

---
+++ EmployeeSQL/sql_hmw_data_eng_schema.sql
@@ -0,0 +1,139 @@
+-- THIS FILE INCLUDES DATA ENGINEERING - TABLE SCHEMATA ONLY.
+
+DROP TABLE IF EXISTS departments;
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS employees;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS salaries;
+DROP TABLE IF EXISTS titles;
+
+-- CREATE CSV Tables and then import data from csv files
+
+CREATE TABLE departments (
+    dept_no VARCHAR(255) NOT NULL ,
+    dept_name VARCHAR(255) NOT NULL,
+	PRIMARY KEY (dept_no)
+	);
+
+CREATE TABLE employees (
+    emp_no INT NOT NULL,
+    emp_title_id VARCHAR(255) NOT NULL,
+	birth_date DATE NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    sex VARCHAR(255) NOT NULL,
+    hire_date DATE NOT NULL,
+	FOREIGN KEY(emp_title_id) REFERENCES Titles (emp_title_id),
+	PRIMARY KEY(emp_no)
+	);
+
+CREATE TABLE dept_emp (
+    emp_no INT NOT NULL,
+    dept_no VARCHAR(255) NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments (dept_no)
+);
+
+
+
+CREATE TABLE Dept_Manager (
+    dept_no VARCHAR(255) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)
+);
+
+CREATE TABLE Salaries (
+    emp_no INT NOT NULL,
+    salary INT NOT NULL,
+	FOREIGN KEY(emp_no) REFERENCES employees (emp_no)  
+);
+
+CREATE TABLE Titles (
+    emp_title_id VARCHAR(255) NOT NULL,
+	title VARCHAR(255) NOT NULL,
+    PRIMARY KEY(emp_title_id)
+);
+
+-- Select all data from each SQL table:
+SELECT * FROM departments;
+SELECT * FROM dept_emp;
+SELECT * FROM employees;
+SELECT * FROM dept_manager;
+SELECT * FROM salaries;
+SELECT * FROM titles;
+
+-- FOR REFERENCE:  Data Analysis in Separate File
+
+-- -- With the complete database, evaluate the following:
+
+-- -- 1. List the following details of each employee: employee number, last name, first name, sex, and salary.
+
+-- SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+-- FROM employees
+-- JOIN salaries
+-- ON employees.emp_no = salaries.emp_no;
+
+-- -- 2. List first name, last name, and hire date for employees who were hired in 1986.
+
+-- SELECT first_name, last_name, hire_date 
+-- FROM employees
+-- WHERE hire_date BETWEEN '1986-01-01' AND '1987-01-01';
+
+-- -- 3. List the manager of each department with the following information: department number, department name, 
+-- -- the manager's employee number, last name, first name.
+
+-- SELECT departments.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+-- FROM departments
+-- JOIN dept_manager
+-- ON departments.dept_no = dept_manager.dept_no
+-- JOIN employees
+-- ON dept_manager.emp_no = employees.emp_no;
+
+-- -- 4. List the department of each employee with the following information: employee number, last name, first name, 
+-- -- and department name.
+
+-- SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+-- FROM dept_emp
+-- JOIN employees
+-- ON dept_emp.emp_no = employees.emp_no
+-- JOIN departments
+-- ON dept_emp.dept_no = departments.dept_no;
+
+-- -- 5. List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+-- SELECT first_name, last_name, sex
+-- FROM employees
+-- WHERE first_name = 'Hercules'
+-- AND last_name LIKE 'B%';
+
+-- -- 6. List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+-- SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+-- FROM dept_emp
+-- JOIN employees
+-- ON dept_emp.emp_no = employees.emp_no
+-- JOIN departments
+-- ON dept_emp.dept_no = departments.dept_no
+-- WHERE departments.dept_name = 'Sales';
+
+-- -- 7. List all employees in the Sales and Development departments, including their employee number, last name, 
+-- -- first name, and department name.
+
+-- SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+-- FROM dept_emp
+-- JOIN employees
+-- ON dept_emp.emp_no = employees.emp_no
+-- JOIN departments
+-- ON dept_emp.dept_no = departments.dept_no
+-- WHERE departments.dept_name = 'Sales' 
+-- OR departments.dept_name = 'Development';
+
+-- -- 8. In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+
+-- SELECT last_name,
+-- COUNT(last_name) AS "frequency"
+-- FROM employees
+-- GROUP BY last_name
+-- ORDER BY
+-- COUNT(last_name) DESC;
